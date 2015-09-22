8e3f767ad89293a73386532c8633b6cb66d8fb3e
Shreya Bhatia <sbhatia261@gmail.com>
2015-09-22 11:40:32 +0530

    removed unused code and errors related to font loading

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
- Rails.application.config.assets.precompile += %w( font-awesome.min.css fontawesome-webfont.eot fontawesome-webfont.ttf fontawesome-webfont.woff )
+ Rails.application.config.assets.precompile += %w( font-awesome.min.css fontawesome-webfont.eot fontawesome-webfont.ttf )
+# Rails.application.config.assets.precompile += %w( font-awesome.min.css fontawesome-webfont.eot fontawesome-webfont.ttf fontawesome-webfont.woff )
