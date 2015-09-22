a3ea7c99a89e2b998159e5d3b7be00ffbe096c3a
Sachin <sagoyal@q3tech.com>
2015-09-22 16:10:29 +0530

    add enable desable link to company and campaign module

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,34 +2,29 @@
 #
 # Table name: users
 #
-#  id                           :integer          not null, primary key
-#  role_id                      :integer          not null
-#  user_type                    :string
-#  username                     :string           default(""), not null
-#  email                        :string           default(""), not null
-#  encrypted_password           :string           default(""), not null
-#  reset_password_token         :string
-#  reset_password_sent_at       :datetime
-#  remember_created_at          :datetime
-#  sign_in_count                :integer          default(0), not null
-#  current_sign_in_at           :datetime
-#  last_sign_in_at              :datetime
-#  current_sign_in_ip           :string
-#  last_sign_in_ip              :string
-#  deleted_at                   :datetime
-#  created_at                   :datetime         not null
-#  updated_at                   :datetime         not null
-#  company_id                   :integer
-#  status                       :boolean
-#  confirmation_token           :string
-#  confirmed_at                 :datetime
-#  confirmation_sent_at         :datetime
-#  skip_confirm_change_password :boolean          default(FALSE)
+#  id                     :integer          not null, primary key
+#  role_id                :integer          not null
+#  user_type              :string
+#  username               :string           default(""), not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  deleted_at             :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  company_id             :integer
+#  status                 :boolean
 #
 # Indexes
 #
 #  index_users_on_company_id            (company_id)
-#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
