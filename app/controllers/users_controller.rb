8321ffbc7812320abb7568196afe69de4442e921
Shreya Bhatia <sbhatia261@gmail.com>
2015-10-15 11:31:00 +0530

    handling destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -85,7 +85,7 @@ class UsersController < ApplicationController
 
 	# Never trust parameters from the scary internet, only allow the white list through.
 	def user_params
-	  params.require(:user).permit(:username, :email, :status, :password, :password_confirmation, :role_id, :image)
+	  params.require(:user).permit(:username, :email, :status, :password, :password_confirmation, :role_id, :image, :company_id)
 	end
 
 end
