de9b2decf238c326b46f838e6d6114a7d92519aa
Shreya Bhatia <sbhatia261@gmail.com>
2015-11-04 10:44:53 +0530

    test drectory

--- test/models/gender_test.rb
+++ test/models/gender_test.rb
@@ -1,26 +1,16 @@
 # == Schema Information
 #
-# Table name: templates
+# Table name: genders
 #
 #  id         :integer          not null, primary key
 #  name       :string
-#  content    :text
-#  status     :boolean
-#  created_by :integer
-#  updated_by :integer
-#  deleted_at :datetime
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  company_id :integer
-#
-# Indexes
-#
-#  index_templates_on_company_id  (company_id)
 #
 
 require 'test_helper'
 
-class TemplateTest < ActiveSupport::TestCase
+class GenderTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
