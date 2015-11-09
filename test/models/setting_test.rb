f7df6a7e520b377d5dab6309c9e3b7d1f67cd412
Shreya Bhatia <sbhatia261@gmail.com>
2015-11-09 10:48:46 +0530

    Test folder

--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -3,17 +3,12 @@
 # Table name: settings
 #
 #  id                   :integer          not null, primary key
-#  user_id              :integer
 #  site_title           :string
+#  free_emails          :string
 #  admin_email          :string
 #  admin_footer_content :string
-#  deleted_at           :datetime
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
-#
-# Indexes
-#
-#  index_settings_on_user_id  (user_id)
 #
 
 require 'test_helper'
