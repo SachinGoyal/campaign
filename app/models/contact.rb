f537149b3304bb043cc9b6c43d9b049fdfbd5a54
Sachin <sagoyal@q3tech.com>
2015-12-14 17:27:35 +0530

    Design report and stat

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -35,8 +35,8 @@ class Contact < ActiveRecord::Base
   validates_presence_of :email, length: { in: 3..255}
  # validates_presence_of :profile_id, length: { in: 3..255}
   # validates_presence_of :profile_ids, message: 'Please select atleast one'
-  validates_uniqueness_to_tenant :email
-  validates_format_of :email, :with => Devise.email_regexp
+#  validates_uniqueness_to_tenant :email
+ # validates_format_of :email, :with => Devise.email_regexp
   validates_inclusion_of :status, in: [true, false]
   # validates_inclusion_of :gender, in: [true, false], message: "Should either be male or female"
   #validation
