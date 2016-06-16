f41c57710b11866ebf662f94d1604b0b593841f3
Shreya Bhatia <sbhatia1@q3tech.com>
2016-06-16 10:01:44 +0530

    logs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,6 +15,21 @@ namespace :deploy do
   after :finishing, 'deploy:update_cron'
 end
 
+namespace :logs do
+  task :tail, :file do |t, args|
+    if args[:file]
+      on roles(:app) do
+        execute "tail -f #{shared_path}/log/#{args[:file]}.log"
+      end
+    else
+      puts "please specify a logfile e.g: 'rake logs:tail[logfile]"
+      puts "will tail 'shared_path/log/logfile.log'"
+      puts "remember if you use zsh you'll need to format it as:"
+      puts "rake 'logs:tail[logfile]' (single quotes)"
+    end
+  end
+end
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
