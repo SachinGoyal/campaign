5de3bf8eb9d91e39afe41430db0a8d4de9dc67d1
Nitin Srivastava <nksrivastava@q3tech.com>
2016-06-29 12:58:08 +0530

    remove gibbon key

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,5 +1,5 @@
-Gibbon::Request.api_key = "d1c1d99de5000c63f0f73f64b88e762e-us12"
+Gibbon::Request.api_key = "api-key"
 Gibbon::Request.timeout = 15
-GIBBON_KEY = "d1c1d99de5000c63f0f73f64b88e762e-us12"
-V2_URL = "https://us12.api.mailchimp.com/"
+GIBBON_KEY = "gibbon-key"
+V2_URL = "v2-url"
 DEFAULT_TEMPLATE_ID = 39645
